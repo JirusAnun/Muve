2e320e5b086d87c4cd5c7cbf9bb2cb54850c8299
Jiruschai Anuntabundit <hamjirus@gmail.com>
2024-05-13 15:40:47 +0700

    add padding to coin

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -2,14 +2,12 @@ import { BsCoin } from "react-icons/bs";
 
 function Coin() {
   return (
-    <>
-      <div className="flex justify-end w-auto mt-6">
-        <div className="bg-black inline-flex items-center p-2 rounded-xl w-auto h-[3vh]">
-          <BsCoin className=" text-yellow-600" />
-          <p className="font-noto font-medium pt-[2px] text-white ml-2">1,000</p>
-        </div>
+    <div className="flex justify-end w-auto mt-5">
+      <div className="bg-black inline-flex items-center p-2 rounded-xl w-auto h-[4vh]">
+        <BsCoin className=" text-yellow-600" />
+        <p className="font-noto font-medium pt-[2px] text-white ml-2">1,000</p>
       </div>
-    </>
+    </div>
   );
 }
 
